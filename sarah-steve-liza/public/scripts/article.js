68d6e3a7da7667b89195eb874a8b2e35e6426a7a
Victoria Oh <veoh1989@gmail.com>
2017-12-18 13:23:30 -0800

    removed white space and commented out code

--- sarah-steve-liza/public/scripts/article.js
+++ sarah-steve-liza/public/scripts/article.js
@@ -22,9 +22,6 @@ var app = app || {};
 
   Article.loadAll = rawData => {
     rawData.sort((a,b) => (new Date(b.publishedOn)) - (new Date(a.publishedOn)))
-    console.log(rawData,'hi')
-    //OLD forEach():
-    //rawData.forEach(articleObject => Article.all.push(new Article(articleObject)));
 
     Article.all = rawData.map(ele => new Article(ele));
 
